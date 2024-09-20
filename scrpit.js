79797bee5b119ba984c50c6dd303c03b7488e900
Melaozin <132454734+Melaozin@users.noreply.github.com>
2024-09-20 00:26:17 +0000

    atualização

--- scrpit.js
+++ scrpit.js
@@ -7,48 +7,70 @@ const displayOdds = async () => {
   const res = await fetch(endPoint);
   const data = await res.json();
 
-  const features = data.events[0].competitions;
+  // Mapeando cada evento em um objeto de jogo
+  const jogos = data.events.map((event) => ({
+    Data: event.date,
+    OddHome: event.competitions[0].odds[0].homeTeamOdds.summary,
+    OddAway: event.competitions[0].odds[0].awayTeamOdds.summary,
+    nomeHome: event.competitions[0].competitors[0].team.shortDisplayName,
+    nomeAway: event.competitions[0].competitors[1].team.shortDisplayName,
+    escudoHome: event.competitions[0].competitors[0].team.logo,
+    escudoAway: event.competitions[0].competitors[1].team.logo,
+    logoLiga: data.leagues[0].logos[0].href,
+    nomeLiga: data.leagues[0].name,
+  }));
 
-  console.log(data);
+  console.log("Jogos: ", jogos);
+  return jogos;
 };
-
-displayOdds();
 
 // Função que cria os quadros de jogos
 const displayInfos = async () => {
   const main = document.getElementById("main");
+  const jogos = await displayOdds();
 
-  games.forEach((game) => {
+  jogos.forEach((infos) => {
+    // Convertendo odds de fração para número
+    const [numeratorHome, denominatorHome] =
+      infos.OddHome.split("/").map(Number);
+    const [numeratorAway, denominatorAway] =
+      infos.OddAway.split("/").map(Number);
+
+    const oddHomeInt = numeratorHome / denominatorHome;
+    const oddAwayInt = numeratorAway / denominatorAway;
+
     const containerHTML = `
-        <div class="game">
-          <span class="header">
-            <img src="${game.leagueImg}" alt="" width="25px" />
-            <p>${game.league}</p>
-            <button>Add</button>
-          </span>
-  
-          <div class="conteudo">
-            <div class="teams team-home">
-              <img src="${game.homeTeam.logo}" alt="" width="70px" />
-              <h3>${game.homeTeam.name}</h3>
-              <span class="team-home_odd">${game.homeTeam.odd.toFixed(2)}</span>
-            </div>
-  
-            <div class="score">
-              <span>
-                <h1>${game.score[0]}</h1>
-                <h1>-</h1>
-                <h1>${game.score[1]}</h1>
-              </span>
-            </div>
-            <div class="teams team-away">
-              <img src="${game.awayTeam.logo}" alt="" width="70px" />
-              <h3>${game.awayTeam.name}</h3>
-              <span class="team-away_odd">${game.awayTeam.odd.toFixed(2)}</span>
-            </div>
-          </div>
+      <div class="game">
+        <div class="header">
+          <img src="${infos.logoLiga}" alt="" width="25px" />
+          <p>${infos.nomeLiga}</p>
+          <button>Add</button>
         </div>
-      `;
+
+        <div class="conteudo">
+          <div class="teams team-home">
+            <img src="${infos.escudoHome}" width="70px" />
+            <h3>${infos.nomeHome}</h3>
+            <span class="team-home_odd">${oddHomeInt.toFixed(2)}</span>
+          </div>
+
+          <div class="score disable">
+            <span>
+              <h1></h1>
+              <h1>-</h1>
+              <h1></h1>
+            </span>
+          </div>
+          <div class="date">${new Date(infos.Data).toLocaleString()}</div>
+        </div>
+        
+        <div class="teams team-away">
+          <img src="${infos.escudoAway}" width="70px" />
+          <h3>${infos.nomeAway}</h3>
+          <span class="team-away_odd">${oddAwayInt.toFixed(2)}</span>
+        </div>
+      </div>
+    `;
 
     // Convertendo a string HTML em um elemento DOM
     const container = document.createElement("div");
@@ -60,4 +82,4 @@ const displayInfos = async () => {
 };
 
 // Chame a função para exibir as informações
-// displayInfos();
+displayInfos();
