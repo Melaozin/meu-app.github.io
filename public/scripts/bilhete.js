f9db0d6f47dd48b4f359560bf0e2ef5e3ecb4eda
Melaozin <132454734+Melaozin@users.noreply.github.com>
2024-09-26 12:00:21 +0000

    adicionado a criação do bilhete e o cálculo de Dutching

--- public/scripts/bilhete.js
+++ public/scripts/bilhete.js
@@ -34,116 +34,50 @@ const excluirJogo = async (id, container) => {
   }
 };
 
-// Função para calcular as apostas e exibir o painel de lucro
-const calcularApostas = () => {
-  const totalStake = parseFloat(document.getElementById("money").value);
-  const jogos = document.querySelectorAll(".game");
-  const infosLucroContainer = document.getElementById("infos-lucro");
-  infosLucroContainer.innerHTML = ""; // Limpa o conteúdo anterior
-
-  let totalApostado = 0;
-  let lucros = [];
-  let totalLucro = 0; // Novo total de lucros
-
-  jogos.forEach((game) => {
-    const oddHome = parseFloat(
-      game.querySelector("span.team-home_odd").innerText
-    );
-    const oddAway = parseFloat(
-      game.querySelector("span.team-away_odd").innerText
-    );
-
-    if (!isNaN(totalStake) && totalStake > 0) {
-      let totalOdds = 1 / oddHome + 1 / oddAway;
-      let stake1 = ((1 / oddHome) * (totalStake / totalOdds)).toFixed(2);
-      let stake2 = ((1 / oddAway) * (totalStake / totalOdds)).toFixed(2);
-      let lucro1 = (oddHome * stake1 - stake1).toFixed(2);
-      let lucro2 = (oddAway * stake2 - stake2).toFixed(2);
-
-      totalApostado += parseFloat(stake1) + parseFloat(stake2);
-      totalLucro += parseFloat(lucro1) + parseFloat(lucro2); // Acumula lucros
-
-      lucros.push({
-        jogo: `${game.querySelector(".team-home h3").innerText} x ${
-          game.querySelector(".team-away h3").innerText
-        }`,
-        lucro: lucro1,
-      });
-      lucros.push({
-        jogo: `${game.querySelector(".team-away h3").innerText} x ${
-          game.querySelector(".team-home h3").innerText
-        }`,
-        lucro: lucro2,
-      });
-    }
-  });
-
-  // Exibir o painel de lucro
-  const lucroTotal = (totalStake - totalLucro) * -1; // Garantir que seja positivo
-  const lucroHTML = `
-    <h3>Lucro</h3>
-    <ul>
-      <p>Lucro Individual:</p>
-      ${lucros
-        .map((item) => `<li>${item.jogo} = R$ ${item.lucro}</li>`)
-        .join("")}
-    </ul>
-    <p>Valor Total do Lucro: R$ ${totalLucro.toFixed(2)}</p>
-    <p>Lucro Total: R$ ${lucroTotal.toFixed(2)}</p>
-  `;
-
-  infosLucroContainer.innerHTML = lucroHTML;
-
-  // Exibir o total apostado
-  const totalApostadoDiv = document.createElement("div");
-  totalApostadoDiv.innerHTML = `<p>Valor Total Apostado: R$ ${totalApostado.toFixed(
-    2
-  )}</p>`;
-  infosLucroContainer.appendChild(totalApostadoDiv);
-};
-
-// Função para exibir os jogos com cálculo automático das odds
+// Função para exibir os jogos
 function display(infos) {
   const main = document.getElementById("main");
   const placarDisplay =
     infos.placarHome === "N/A" || infos.placarAway === "N/A" ? "none" : "flex";
 
-  // Criando os elementos do painel do jogo
-  const gameElement = document.createElement("div");
-  gameElement.classList.add("game");
-  gameElement.dataset.id = infos.id;
+  // Criando o HTML do painel do jogo
+  const containerHTML = `
+    <div class="game" data-id="${infos.id}">
+      <div class="header">
+        <img src="${infos.ligaLogo}" alt="" width="25px" />
+        <p>${infos.liga}</p>
+        <button class="delete-btn">Excluir</button>
+      </div>
+      <div class="conteudo">
+        <div class="teams team-home">
+          <img src="${infos.logoHome}" width="70px" />
+          <h3>${infos.nomeHome}</h3>
+          <span class="team-home_odd">${infos.oddHome}</span>
+        </div>
 
-  gameElement.innerHTML = `
-    <div class="header">
-      <img src="${infos.ligaLogo}" alt="" width="25px" />
-      <p>${infos.liga}</p>
-      <button class="delete-btn">Excluir</button>
-    </div>
-    <div class="conteudo">
-      <div class="teams team-home">
-        <img src="${infos.logoHome}" width="70px" />
-        <h3>${infos.nomeHome}</h3>
-        <span class="team-home_odd">${infos.oddHome}</span>
-      </div>
-      <div class="score">
-        <div class="placar" style="display: ${placarDisplay};">
-          <h1>${infos.placarHome}</h1>
-          <h2>x</h2>
-          <h1>${infos.placarAway}</h1>
+        <div class="score">
+          <div class="placar" style="display: ${placarDisplay};">
+            <h1>${infos.placarHome}</h1>
+            <h2>x</h2>
+            <h1>${infos.placarAway}</h1>
+          </div> 
+          <div class="date">
+            <h4>${infos.status}</h4>
+          </div>
         </div>
-        <div class="date">
-          <p class="elemento"></p>
-          <h4>${infos.status}</h4>
+        
+        <div class="teams team-away">
+          <img src="${infos.logoAway}" width="70px" />
+          <h3>${infos.nomeAway}</h3>
+          <span class="team-away_odd">${infos.oddAway}</span>
         </div>
-        <span class="aposta-text">Aposta</span>
-      </div>
-      <div class="teams team-away">
-        <img src="${infos.logoAway}" width="70px" />
-        <h3>${infos.nomeAway}</h3>
-        <span class="team-away_odd">${infos.oddAway}</span>
       </div>
     </div>
   `;
+
+  const container = document.createElement("div");
+  container.innerHTML = containerHTML;
+  const gameElement = container.firstElementChild;
 
   // Adicionar evento de clique ao botão de excluir
   const deleteBtn = gameElement.querySelector(".delete-btn");
@@ -154,10 +88,6 @@ function display(infos) {
   // Adicionar o painel do jogo ao elemento principal (main)
   main.appendChild(gameElement);
 }
-
-// Evento do botão de calcular apostas
-const calcularBtn = document.getElementById("calcularBtn"); // Certifique-se de que este botão existe no HTML
-calcularBtn.addEventListener("click", calcularApostas);
 
 // Chamar a função para ler os jogos e exibi-los
 lerJogos();
--- public/scripts/testes.js
+++ public/scripts/testes.js
@@ -1,43 +1,189 @@
-let odd1;
-let odd2;
+// Função para ler os jogos e exibi-los na interface
+const lerJogos = async () => {
+  try {
+    const res = await fetch("/ler-jogos");
+    if (!res.ok) throw new Error("Erro ao ler os jogos");
 
-function calcularDutching() {
-  const totalStake = parseFloat(document.getElementById("totalStake").value);
-  const odds = document.querySelectorAll(".odd");
-  const resultados = document.getElementById("resultados");
-  resultados.innerHTML = "";
-  const lucro1 = document.getElementById("lucro1");
-  const lucro2 = document.getElementById("lucro2");
+    const jogos = await res.json();
 
-  let totalOdds = 0;
-  let stakePerSelection = [];
+    // Limpar a interface antes de exibir os novos jogos
+    const main = document.getElementById("main");
+    main.innerHTML = "";
 
-  odds.forEach((oddInput) => {
-    const odd = parseFloat(oddInput.value);
-    totalOdds += 1 / odd;
-    stakePerSelection.push(1 / odd);
+    // Exibir os jogos na interface
+    jogos.forEach((infos) => {
+      display(infos);
+    });
+  } catch (error) {
+    console.error("Erro ao ler os jogos:", error);
+  }
+};
 
-    odd1 = parseFloat(odds[0].value); // Armazena o valor da primeira odd na variável odd1.
-    odd2 = parseFloat(odds[1].value); // Armazena o valor da segunda odd na variável odd2.
+// Função para excluir o jogo
+const excluirJogo = async (id, container) => {
+  try {
+    const res = await fetch(`/delete-jogo/${id}`, { method: "DELETE" });
+    if (!res.ok) throw new Error("Erro ao excluir o jogo");
+
+    // Remove o painel do jogo da interface
+    container.remove();
+  } catch (error) {
+    console.error("Erro ao excluir o jogo:", error);
+  }
+};
+
+// Função para calcular as apostas
+function calcularApostas() {
+  const totalStake = parseFloat(document.getElementById("money").value);
+  const games = document.querySelectorAll(".game");
+  const oddsArray = [];
+  const jogosInfo = [];
+
+  games.forEach((game) => {
+    const oddHome = game.querySelector("span.team-home_odd").innerHTML;
+    const oddAway = game.querySelector("span.team-away_odd").innerHTML;
+    const jogo = `${game.querySelector(".team-home h3").innerHTML} x ${
+      game.querySelector(".team-away h3").innerHTML
+    }`;
+
+    const stage2 = 1 / oddHome + 1 / oddAway;
+    const stage1Home = totalStake / oddHome;
+    const stage1Away = totalStake / oddAway;
+
+    const ViHome = (stage1Home / stage2).toFixed(2);
+    const ViAway = (stage1Away / stage2).toFixed(2);
+
+    oddsArray.push(ViAway, ViHome);
+    jogosInfo.push(jogo);
   });
 
-  const stakeMultiplier = totalStake / totalOdds;
-
-  stakePerSelection.forEach((stake, index) => {
-    const stakeAmount = (stake * stakeMultiplier).toFixed(2);
-    resultados.innerHTML += `<li>Time ${
-      index + 1
-    }: Valor que deve apostar R$ ${stakeAmount}</li>`;
-
-    if (index === 0) {
-      valor1 = stakeAmount;
-    }
-    if (index === 1) {
-      valor2 = stakeAmount;
-    }
-  });
-  const lucro_1 = (odd1 * valor1 - totalStake).toFixed(2);
-  lucro1.innerHTML = `<li>Se o Time 1 ganhar:</li> <ol>Seu lucro será de: R$ ${lucro_1}</ol>`; // Mostrar o lucro
-  const lucro_2 = (odd1 * valor1 - totalStake).toFixed(2);
-  lucro2.innerHTML = `<li>Se o Time 2 ganhar:</li> <ol>Seu lucro será de: R$ ${lucro_2}</ol>`; // Mostrar o lucro
+  return [oddsArray, totalStake, jogosInfo, games];
 }
+
+// Função para exibir os jogos
+function display(infos) {
+  const main = document.getElementById("main");
+  const placarDisplay =
+    infos.placarHome === "N/A" || infos.placarAway === "N/A" ? "none" : "flex";
+
+  // Criando o HTML do painel do jogo
+  const containerHTML = `
+    <div class="game" data-id="${infos.id}">
+      <div class="header">
+        <img src="${infos.ligaLogo}" alt="" width="25px" />
+        <p>${infos.liga}</p>
+        <button class="delete-btn">Excluir</button>
+      </div>
+      <div class="conteudo">
+        <div class="teams team-home">
+          <img src="${infos.logoHome}" width="70px" />
+          <h3>${infos.nomeHome}</h3>
+          <span class="team-home_odd">${infos.oddHome}</span>
+        </div>
+
+        <div class="score">
+          <div class="placar" style="display: ${placarDisplay};">
+            <h1>${infos.placarHome}</h1>
+            <h2>x</h2>
+            <h1>${infos.placarAway}</h1>
+          </div> 
+          <div class="date">
+            <h4>${infos.status}</h4>
+          </div>
+        </div>
+        
+        <div class="teams team-away">
+          <img src="${infos.logoAway}" width="70px" />
+          <h3>${infos.nomeAway}</h3>
+          <span class="team-away_odd">${infos.oddAway}</span>
+        </div>
+      </div>
+    </div>
+  `;
+
+  const container = document.createElement("div");
+  container.innerHTML = containerHTML;
+  const gameElement = container.firstElementChild;
+
+  // Adicionar evento de clique ao botão de excluir
+  const deleteBtn = gameElement.querySelector(".delete-btn");
+  deleteBtn.addEventListener("click", () => {
+    excluirJogo(infos.id, gameElement);
+  });
+
+  // Adicionar o painel do jogo ao elemento principal (main)
+  main.appendChild(gameElement);
+}
+
+const calcularBtn = document.querySelector("#calcularBtn");
+calcularBtn.addEventListener("click", () => {
+  const [odds, totalStake, jogosInfo, games] = calcularApostas();
+  const painelLucro = document.getElementById("infos-lucro");
+
+  const oddsHome = [];
+  const oddsAway = [];
+
+  games.forEach((game) => {
+    const oddHome = game.querySelector("span.team-home_odd").innerHTML;
+    oddsHome.push(oddHome);
+  });
+  games.forEach((game) => {
+    const oddAway = game.querySelector("span.team-away_odd").innerHTML;
+    oddsAway.push(oddAway);
+  });
+
+  // Create HTML for individual profits
+  const apostaItems = jogosInfo
+    .map((jogo, index) => {
+      return `<li> (R$ ${odds[index * 2 + 1]}) ${jogo} (R$ ${
+        odds[index * 2]
+      }), </li>`;
+    })
+    .join("");
+
+  // Criar HTML para lucros
+  const lucroItems = jogosInfo
+    .map((jogo, index) => {
+      return `<li>(R$ ${(
+        oddsHome[index] * odds[index * 2 + 1] -
+        totalStake
+      ).toFixed(2)}) ${jogo}  (R$ ${(
+        oddsAway[index] * odds[index * 2] -
+        totalStake
+      ).toFixed(2)})</li>`;
+    })
+    .join("");
+
+  const retornoItems = jogosInfo
+    .map((jogo, index) => {
+      return `<li>(R$ ${(oddsHome[index] * odds[index * 2 + 1]).toFixed(
+        2
+      )}) ${jogo}  (R$ ${(oddsAway[index] * odds[index * 2]).toFixed(2)})</li>`;
+    })
+    .join("");
+
+  const lucroHtml = `
+  <h3>Lucro</h3>
+  <ul>
+    <p>Aposta Individual:</p>
+    ${apostaItems}
+  </ul>
+  <ul>
+    <p>Possíveis retornos:</p>
+    ${retornoItems}
+  </ul>
+  <ul>
+    <p>Aposta Individual:</p>
+    ${lucroItems}
+  </ul>
+  <ul>
+    <p>Valor Apostado:</p>
+    <li>R$ ${totalStake.toFixed(2)}</li>
+  </ul>
+  `;
+
+  painelLucro.innerHTML = lucroHtml;
+});
+
+// Chamar a função para ler os jogos e exibi-los
+lerJogos();
